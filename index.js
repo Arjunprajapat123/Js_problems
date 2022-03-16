55be793ccaeb68f8610deeb66e5a61e03c05cf31
Arjun <arjunprajapat420@gmail.com>
2022-03-16 13:01:37 +0530

    switch case

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("My name is Arjun Prajapat , this is demo code to upload on github")
+// switch case 
+
+var day = 2;
+switch(day){
+    case 1 : console.log("today is monday")
+    break;
+    case 2 : console.log("today is tuesday")
+    break;
+    case 3 : console.log("today is wed.")
+    break;
+    case 4 : console.log("today is thur.")
+    break;
+    case 5 : console.log("today is fri.")
+    break;
+    case 6 : console.log("today is sut.")
+    break;
+    case 7 : console.log("today is san.")
+    break;
+}
